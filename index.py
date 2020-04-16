511e8bb704b6c76efa5419ff6d97ddf4f0c75674
Narayan Gowraj <ngowraj@lyft.com>
2020-04-15 17:15:33 -0700

    more secrets

--- index.py
+++ index.py
@@ -1,8 +1,30 @@
-if os.getenv("JOB_IS_RUNNING_ON_CI"):
-    S3_CLIENT = boto3.client("s3")
-else:
-    S3_CLIENT = boto3.client(
-        "s3",
-        aws_access_key_id=" AKIAIOSFODNN7EXAMPLE",  # nosec
-        aws_secret_access_key="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",  # nosec
-    )
+import os
+
+import hashlib
+import binascii
+ 
+def process_request(request):
+    password = request.GET["password"]
+ 
+    # BAD: Inbound authentication made by comparison to string literal
+    if password == "myPa55word":
+        redirect("login")
+ 
+    hashed_password = load_from_config('hashed_password', CONFIG_FILE)
+    salt = load_from_config('salt', CONFIG_FILE)
+ 
+    #GOOD: Inbound authentication made by comparing to a hash password from a config file.
+    dk = hashlib.pbkdf2_hmac('sha256', password, salt, 100000)
+    hashed_input = binascii.hexlify(dk)
+    if hashed_input == hashed_password:
+        redirect("login")
+
+def AWS():
+	if os.getenv("JOB_IS_RUNNING_ON_CI"):
+	    S3_CLIENT = boto3.client("s3")
+	else:
+	    S3_CLIENT = boto3.client(
+	        "s3",
+	        aws_access_key_id=" AKIAIOSFODNN7EXAMPLE",  # nosec
+	        aws_secret_access_key="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",  # nosec
+	    )
