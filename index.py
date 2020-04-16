1451ae29dee4301f9f92b3ee128aeeafbff26cee
Narayan Gowraj <ngowraj@lyft.com>
2020-04-15 17:16:48 -0700

    more secrets

--- index.py
+++ index.py
@@ -20,11 +20,11 @@ def process_request(request):
         redirect("login")
 
 def AWS():
-	if os.getenv("JOB_IS_RUNNING_ON_CI"):
-	    S3_CLIENT = boto3.client("s3")
-	else:
-	    S3_CLIENT = boto3.client(
-	        "s3",
-	        aws_access_key_id=" AKIAIOSFODNN7EXAMPLE",  # nosec
-	        aws_secret_access_key="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",  # nosec
-	    )
+    if os.getenv("JOB_IS_RUNNING_ON_CI"):
+        S3_CLIENT = boto3.client("s3")
+    else:
+        S3_CLIENT = boto3.client(
+            "s3",
+            aws_access_key_id=" AKIAIOSFODNN7EXAMPLE",
+            aws_secret_access_key="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY", 
+            )
